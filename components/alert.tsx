55323d335acad031a0d881ad1a13f31418ce24e5
Whitman Schorn <whitman.schorn@gmail.com>
2023-08-02 12:21:11 -0500

    cleanup

--- components/alert.tsx
+++ components/alert.tsx
@@ -3,6 +3,9 @@ import cn from 'classnames'
 import { EXAMPLE_PATH } from '../lib/constants'
 
 export default function Alert({ preview }) {
+
+  if(!preview) return <></>
+    
   return (
     <div
       className={cn('border-b', {
@@ -12,7 +15,7 @@ export default function Alert({ preview }) {
     >
       <Container>
         <div className="py-2 text-center text-sm">
-          {preview ? (
+          {preview && 
             <>
               This is a page preview.{' '}
               <a
@@ -23,18 +26,7 @@ export default function Alert({ preview }) {
               </a>{' '}
               to exit preview mode.
             </>
-          ) : (
-            <>
-              The source code for this blog is{' '}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
-            </>
-          )}
+          }
         </div>
       </Container>
     </div>
