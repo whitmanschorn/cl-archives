b56851f1501585a8a398c4430fbaaa338df422f3
Whitman Schorn <whitman.schorn@gmail.com>
2023-08-02 12:24:25 -0500

    cleanup ii

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,20 +7,20 @@ export default function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+            Copyright © 2012 – 2023 Chump Lady
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://nextjs.org/docs/basic-features/pages"
+              href="https://www.patreon.com/chumplady"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Read Documentation
+              Support on Patreon
             </a>
             <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+              href={`https://www.amazon.com/Leave-Cheater-Gain-Life-Survival/dp/0762458968`}
               className="mx-3 font-bold hover:underline"
             >
-              View on GitHub
+              Buy the book
             </a>
           </div>
         </div>
